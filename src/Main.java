6b44b2166cda8a4f0020baae4e916e6a9cc70b7f
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-06-07 12:54:01 +0530

    logging framework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger logger = Logger.getInstance();
+
+        //logging with default configuration
+        logger.info("This is an information message");
+        logger.warning("This is an warning message");
+        logger.error("This is an error message");
+
+        //changing log level and appender
+        LoggerConfig config = new LoggerConfig(LogLevel.DEBUG,new FileAppender("app.log"));
+        logger.setConfig(config);
+
+        logger.debug("This is a debug message");
+        logger.info("This is a information message");
     }
 }
